57467ba17d380323c7c7c2f96f87e471ec072162
John Phillips <john.phillips5@hpe.com>
2016-09-08 17:50:56 +0000

    add python files

--- dokuwiki.py
+++ dokuwiki.py
@@ -3,6 +3,7 @@ import sys
 import xml.etree.ElementTree as ET
 import getpass
 import argparse
+import xmlrpc
 
 class DokuWikiError(RuntimeError):
     def __init__(self, string, xml, response):
@@ -29,89 +30,40 @@ class DokuWiki:
     https://en.wikipedia.org/wiki/XML-RPC
     https://www.dokuwiki.org/devel:xmlrpc
     '''
-    XML_RPC=(u'''
-    <?xml encoding="utf8" version="1.0"?>
-    <methodCall>
-      <methodName>{method_name}</methodName>
-      <params>
-        {parameters}
-      </params>
-    </methodCall>''')
 
     RPC_PATH="/lib/exe/xmlrpc.php"
 
-    def __get_parsed_response(self, method, params, retrieve_path):
-        params_str = ''
-        # technically, there are other types of parameters defined in the
-        # XML-RPC. I don't use any so I don't check.
-
-        for param in params:
-            # CDATA tag means 'character data' and is very much necessary
-            # for the frequent case of embedded HTML markup.
-            params_str += "<param><string><![CDATA[{}]]></string></param>".format(param)
-
-        data_str = DokuWiki.XML_RPC.format(method_name=method,
-                                           parameters=params_str)
-
-        response = requests.post(self.url + DokuWiki.RPC_PATH,
-                                data=data_str.encode('utf8', errors='ignore'),
-                                cookies=self.cookies,
-                                verify=self.verify,
-                                allow_redirects=False)
-
-        if not (response.status_code >= 200 and
-                response.status_code <= 299):
-            raise DokuWikiError("response code was not OK.",
-                                data_str,
-                                response)
-
-        return_val = None
-
-        try:
-            # XML should be able to use UTF-8. However,
-            # the dokuwiki doesn't send an 'encoding' parameter in it's
-            # xml header or whatever it is. ElemTree is confused
-            # by utf8 chars and throws an error.
-            parsed_response = ET.fromstring(
-                    response.text.encode('utf-8', errors='ignore'))
-            return_val = parsed_response.findall(retrieve_path)[0].text
-        except Exception as e:
-            raise DokuWikiError(
-                "caught {}: {}".format(e.__class__, e),
-                data_str,
-                response)
-
-        return return_val if return_val is not None else '', response
-
     def __init__(self, url, verify=True):
-        self.url=url
+        self.url=url+DokuWiki.RPC_PATH
         self.version = None
-        self.verify = verify
-        self.cookies = requests.cookies.RequestsCookieJar()
-        self.version = self.__get_parsed_response(
-                            'dokuwiki.getVersion',
-                            [],
-                            './params/param/value/string')
+        self.rpc_client = xmlrpc.XMLRPCClient(self.url, verify=verify)
+        self.version = self.rpc_client.call('dokuwiki.getVersion')[0]
 
     def get_page(self, pagename):
-        return self.__get_parsed_response(
-                'wiki.getPage',
-                [pagename],
-                './params/param/value/string')[0]
+        return self.rpc_client.call('wiki.getPage', pagename)[0]
 
     def put_page(self, pagename, pagetext):
-        return bool(int(self.__get_parsed_response(
-                        'wiki.putPage',
-                        [pagename, pagetext],
-                        './params/param/value/boolean')[0]))
+        return self.rpc_client.call('wiki.putPage',
+                                    pagename, pagetext)[0]
+    def get_page_html(self, pagename):
+        return self.rpc_client.call('wiki.getPageHTML', pagename)[0]
+
+    def lock(self, page):
+        result = self.rpc_client.call('dokuwiki.setLocks',
+                                    {'lock':
+                                    [page],
+                                    'unlock':
+                                    []})[0]
+        if page in result['locked']:
+            return True
+        elif page in result['lockfail']:
+            return False
+
+
 
     def login(self, username, password):
-        value, response = self.__get_parsed_response(
-                            'dokuwiki.login',
-                            [username, password],
-                            './params/param/value/boolean')
-        self.cookies.update(response.cookies)
-        return bool(int(value))
+        return self.rpc_client.call('dokuwiki.login',
+                                    username, password)[0]
 
 '''
 Demo program for fetching dokuwiki stuff using XML RPC
@@ -145,6 +97,11 @@ def main(url,
         for line in iter(input_file.readline, ''):
             wikitext += line
         wiki.put_page(page, wikitext)
+    elif action == 'lock':
+        if wiki.lock(page):
+            print('OK')
+        else:
+            print("can't lock page " + page)
     else:
         print('undefined action')
 
--- xmlrpc.py
+++ xmlrpc.py
@@ -1,6 +1,11 @@
 import xml.etree.ElementTree as ET
 import requests
 
+
+'''
+Module implementing basic XML RPC calls.
+
+'''
 def _encode_list(list_parameter):
     assert type(list_parameter) == list
     listr = '<array><data>'
@@ -33,7 +38,7 @@ def _encode_parameter(param):
 
 
 encoders = {
-    str:lambda s: '<string>{}</string>'.format(s),
+    str:lambda s: '<string><![CDATA[{}]]></string>'.format(s),
     int:lambda i: '<int>{}</int>'.format(i),
     list:_encode_list,
     dict:_encode_struct,
@@ -42,9 +47,33 @@ encoders = {
 
 
 def encode_parameters(*params):
+    '''
+    Encode parameters that are python data types into XML for an XMLRPC call.
+    parameters may be dictionaries, lists, strings, integers, or booleans, and
+    will be recursively encoded.
+
+    *params -- parameters to be encoded into the params list.
+
+    Returns: XML for an XML RPC call, starting with the <params> tag.
+
+    ex:
+    encode_parameters([1, 2, 3]) -->
+    <params>
+      <param>
+        <array>
+          <value><int>1</int></value>
+          ...
+        </array>
+      </param>
+    </params>
+    '''
     paramstr='<params>'
+    if params == [[]]:
+        return ''
+
     for param in params:
         paramstr += _encode_parameter(param)
+
     return paramstr + '</params>'
 
 
@@ -73,7 +102,7 @@ def _decode_struct(parameter):
         if len(names) != 1:
             raise RuntimeError("not expected number of names: {} {}"
                     .format(len(names), names))
-        
+
         name = names[0].text
         values = member.findall('./value/*')
 
@@ -91,16 +120,34 @@ def _decode_boolean(parameter):
 def _decode_int(parameter):
     return int(parameter.text)
 
+def _decode_value(parameter):
+    value = parameter.findall('./*')[0]
+    return decoders[value.tag](value)
+
 decoders = {
     'array':_decode_array,
     'string':_decode_string,
     'struct':_decode_struct,
     'int':_decode_int,
     'boolean':_decode_boolean,
+    'value':_decode_value,
 }
 
 
 def decode_parameters(raw_parameters):
+    '''
+    Decode raw parameters from a methodResponse.
+
+    raw_parameters -- either an XML string of the parameters, e.g.
+                       <params>
+                         <param>
+                           ...
+                         </param>
+                       </params>
+                       or an already parsed ElementTree.
+
+    Returns: a python data type, a list, string int, dict, or boolean value.
+    '''
     if type(raw_parameters) == str:
         root = ET.fromstring(raw_parameters)
     else:
@@ -119,7 +166,7 @@ def decode_parameters(raw_parameters):
         parameter_type = parameter_types[0]
 
         if not parameter_type.tag in decoders:
-            raise RuntimeError("key {} is not in decoders".format(parameter_type.tag))
+            raise RuntimeError("key {} {} is not in decoders".format(parameter_type.tag, parameter_type.text))
 
         decoded.append(decoders[parameter_type.tag](parameter_type))
 
@@ -129,30 +176,76 @@ def decode_parameters(raw_parameters):
 class XMLRPCClientError(RuntimeError):
     pass
 
+class XMLRPCClientFaultError(RuntimeError):
+    def __init__(self, fault_text, fault, params, request_xml):
+        self._err_str = fault_text + '\nfault:\n' + \
+                '\t' + 'code: ' + str(fault['faultCode']) + '\n' + \
+                '\t' + 'message: ' + str(fault['faultString']) + '\n' + \
+                '\t' + 'params: ' + '\n'
+        for param in params:
+            self._err_str += '\t\t' + str(param) + '\n'
+
+        self._err_str += request_xml
+        self._err_str += '\n'
+
+    def __str__(self):
+        return self._err_str
+
 class XMLRPCClient:
     XML_HEADER='<?xml version="1.0" encoding="utf8"?>'
-    def __init__(self, url):
+    def __init__(self, url, verify=True):
+        '''
+        url    -- url to send XML RPC POST requests to.
+        verify -- whether or not to verify SSL certificates.
+        '''
         self.url = url
+        self.verify=verify
         self.cookies = requests.cookies.RequestsCookieJar()
 
     def call(self, method_name, *params):
+        '''
+        Call method method_name, returning whatever structure it returns,
+        decoded. Throws an exception if:
+            * HTTP return code isn't OK
+            * The method resuponse doesn't have a <params> tag and <param> tags
+            within.
+
+        method_name -- method name to call
+        *params     -- list of parameters to be encoded.
+        '''
         method_text = XMLRPCClient.XML_HEADER + \
                     '<methodCall>' + \
                     '<methodName>' + method_name + '</methodName>' + \
-                    encode_parameters(list(params)) + \
+                    (encode_parameters(*params) if len(params) > 0 else '') + \
                     '</methodCall>'
         response = requests.post(self.url,
-                                 data=method_text)
+                                 data=method_text,
+                                 verify=self.verify,
+                                 cookies=self.cookies)
         if not response.ok:
             raise XMLRPCClientError('Bad return code from server: {}'.format(
                 response.text))
 
         self.cookies.update(response.cookies)
-        #print(response.text)
-        elements = ET.fromstring(response.text)
+        elements = ET.fromstring(response.text.encode(encoding='ASCII', errors='ignore'))
         params = elements.findall('./params')
         if len(params) != 1:
-            raise XMLRPCClientError('There should be exactly one parameter tag: {} {}'.format(
-                params, response.text))
+            fault = elements.findall('./fault/value/struct')
+            if len(fault) != 1:
+                raise XMLRPCClientError('There should be exactly one parameter tag: {} {}'.format(
+                    params, response.text))
+            else:
+                fault_dict = decoders[fault[0].tag](fault[0])
+                raise XMLRPCClientFaultError('Fault calling ' + method_name,
+                                             fault_dict,
+                                             params,
+                                             method_text)
 
-        return decode_parameters(params[0])
+        decoded = None
+        try:
+            decoded = decode_parameters(params[0])
+        except RuntimeError as e:
+            raise XMLRPCClientError("error with xml: {}, {}, {}".format(
+                method_text, response.text, e))
+
+        return decoded
